ec67052f0f2359b49d85f034434a34dafc2dd253
Eric Ghoubiguian <eric.ghoubiguian@imerir.com>
2020-06-25 21:46:36 +0200

    Definition of attributes and properties of the weather class

--- universalWeather/weather.cs
+++ universalWeather/weather.cs
@@ -6,5 +6,75 @@ namespace universalweather.universalWeather
 {
     class weather
     {
+        /****   *****/
+
+        //
+        private float longitude;
+        private float latitude;
+
+        //
+        private string mainDescription;
+        private string precisedDescription;
+
+        //
+        private float temperature;
+        private float feelingLikeTemperature;
+        private float minTemperature;
+        private float maxTemperature;
+
+        //
+        private float pressure;
+        private float humidity;
+
+        //
+        private float windSpeed;
+        private int windDeg;
+        private float windGust;
+
+        //
+        private long sunrise;
+        private long sunset;
+
+        //
+        private string countryCode;
+        private string cityName;
+
+        /****   *****/
+
+        //
+        public float Longitude { get { return this.longitude; } }
+        public float Latitude { get { return this.latitude; } }
+
+        //
+        public string MainDescription { get { return this.mainDescription; } }
+        public string PrecisedDescription { get { return this.precisedDescription; } }
+
+        //
+        public float Temperature { get { return this.temperature; } }
+        public float FeelingLikeTemperature { get { return this.feelingLikeTemperature; } }
+        public float MinTemperature { get { return this.minTemperature; } }
+        public float MaxTemperature { get { return this.maxTemperature; } }
+
+        //
+        public float Pressure { get { return this.pressure; } }
+        public float Humidity { get { return this.humidity; } }
+
+        //
+        public float WindSpeed { get { return this.windSpeed; } }
+        public int WindDeg { get { return this.windDeg; } }
+        public float WindGust { get { return this.windGust; } }
+
+        //
+        public long Sunrise { get { return this.sunrise; } }
+        public long Sunset { get { return this.sunset; } }
+
+        //
+        public string CountryCode { get { return this.countryCode; } }
+        public string CityName { get { return this.cityName; } }
+
+        weather()
+        {
+
+        }
     }
 }
