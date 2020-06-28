ee7d4b1d29e37aefd3a1f5454ecb8fc25127e681
Eric Ghoubiguian <eric.ghoubiguian@imerir.com>
2020-06-28 10:50:59 +0200

    Writing comments

--- universalWeather/weather.cs
+++ universalWeather/weather.cs
@@ -10,75 +10,75 @@ namespace universalweather.universalWeather
     {
         /**** weather class' attributes (to store each relative data)  *****/
 
-        //
+        // Attributes for respectively longitude and latitude
         private float longitude;
         private float latitude;
 
-        //
+        // Attributes for respectively main description and description with more precisions
         private string mainDescription;
         private string precisedDescription;
 
-        //
+        // Attributes for all datas concerning temperature
         private float temperature;
         private float feelingLikeTemperature;
         private float minTemperature;
         private float maxTemperature;
 
-        //
+        // Attributes for respectively pressure and humidity
         private float pressure;
         private float humidity;
 
-        //
+        // Attributes for all datas concerning wind
         private float windSpeed;
         private int windDeg;
         private float windGust;
 
-        //
+        // Attributes for respectively sunrise and sunset times as timestamp
         private long sunrise;
         private long sunset;
 
-        //
+        // Attributes for all datas concerning wished location
         private string countryCode;
         private string cityName;
 
-        //
+        // Attributes for all datas concerning UV
         private int uvIndex;
         private string uvRisk;
 
         /**** weather class' properties (to return each relative data's corresponding value)  *****/
 
-        //
+        // Properties for respectively longitude and latitude
         public float Longitude { get { return this.longitude; } }
         public float Latitude { get { return this.latitude; } }
 
-        //
+        // Properties for respectively main description and description with more precisions
         public string MainDescription { get { return this.mainDescription; } }
         public string PrecisedDescription { get { return this.precisedDescription; } }
 
-        //
+        // Properties for all datas concerning temperature
         public float Temperature { get { return this.temperature; } }
         public float FeelingLikeTemperature { get { return this.feelingLikeTemperature; } }
         public float MinTemperature { get { return this.minTemperature; } }
         public float MaxTemperature { get { return this.maxTemperature; } }
 
-        //
+        // Properties for respectively pressure and humidity
         public float Pressure { get { return this.pressure; } }
         public float Humidity { get { return this.humidity; } }
 
-        //
+        // Properties for all datas concerning wind
         public float WindSpeed { get { return this.windSpeed; } }
         public int WindDeg { get { return this.windDeg; } }
         public float WindGust { get { return this.windGust; } }
 
-        //
+        // Properties for respectively sunrise and sunset times as timestamp
         public long Sunrise { get { return this.sunrise; } }
         public long Sunset { get { return this.sunset; } }
 
-        //
+        // Properties for all datas concerning wished location
         public string CountryCode { get { return this.countryCode; } }
         public string CityName { get { return this.cityName; } }
 
-        //
+        // Properties for all datas concerning UV
         public int UvIndex { get { return this.uvIndex; } }
         public string UvRisk { get { return this.uvRisk; } }
 
